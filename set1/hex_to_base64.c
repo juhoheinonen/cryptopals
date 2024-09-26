5d761c294a78d83550f8d16af010d46b48e66fa9
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2024-09-26 16:21:49 +0300

    Set 1/1 done

--- set1/hex_to_base64.c
+++ set1/hex_to_base64.c
@@ -2,9 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+unsigned char* hex_to_bytes(const char *hex_string, int len);
+char *base64_encode(const unsigned char *data, int input_length);
+
 // convert hex to base64
 // input:  49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
 // output: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+// Function prototype for hex_to_bytes
 int main(int argc, char *argv[])
 {
 	// if argv length is not 2, print usage and exit. Usage should mention that the input is a hex string
@@ -21,26 +25,76 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	unsigned char* bytes = (char*)malloc(len / 2);
-	
+	unsigned char* bytes = hex_to_bytes(argv[1], len);
+		
 	// go through argv[1] and parse two characters at a time to convert to a byte	
 	for (int i = 0; i < len; i += 2) {
 		char byte[3] = {argv[1][i], argv[1][i + 1], '\0'};
 		bytes[i / 2] = strtol(byte, NULL, 16);
-	}		
+	}
 
-	// // loop through the bytes and print the value as integer and add dot between them
-	// for (int i = 0; i < len / 2; i++) {
-	// 	printf("%d", bytes[i]);
-	// 	if (i < len / 2 - 1) {
-	// 		printf(".");
-	// 	}
-	// }	
+	// loop through the bytes and print the value as integer and add dot between them
+	for (int i = 0; i < len / 2; i++) {
+		printf("%d", bytes[i]);
+		if (i < len / 2 - 1) {
+			printf(".");
+		}
+	}	
 
-	// // loop through the bytes and print the value as character
-	// for (int i = 0; i < len / 2; i++) {
-	// 	printf("%c", bytes[i]);
-	// }
+	// base64 encode the bytes
+	char *encoded_data = base64_encode(bytes, len / 2);
 
-	printf("loppu");
+	// print the base64 encoded data
+	printf("\n%s\n", encoded_data);
+
+	// free the memory allocated
+	free(bytes);	
+	free(encoded_data);
+}
+
+// Function to convert a hex string to bytes
+unsigned char* hex_to_bytes(const char *hex_string, int len) {
+    unsigned char *bytes = (unsigned char*)malloc(len / 2);
+    if (bytes == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < len; i += 2) {
+        char byte[3] = {hex_string[i], hex_string[i + 1], '\0'};
+        bytes[i / 2] = strtol(byte, NULL, 16);
+    }
+
+    return bytes;
+}
+
+static const char base64_chars[] = 
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+char *base64_encode(const unsigned char *data, int input_length) {
+    int output_length = 4 * ((input_length + 2) / 3);
+    char *encoded_data = malloc(output_length + 1);
+    if (encoded_data == NULL) return NULL;
+
+    int i, j;
+    for (i = 0, j = 0; i < input_length;) {
+        unsigned int octet_a = i < input_length ? data[i++] : 0;
+        unsigned int octet_b = i < input_length ? data[i++] : 0;
+        unsigned int octet_c = i < input_length ? data[i++] : 0;
+
+        unsigned int triple = (octet_a << 16) + (octet_b << 8) + octet_c;
+
+        encoded_data[j++] = base64_chars[(triple >> 18) & 0x3F];
+        encoded_data[j++] = base64_chars[(triple >> 12) & 0x3F];
+        encoded_data[j++] = base64_chars[(triple >> 6) & 0x3F];
+        encoded_data[j++] = base64_chars[triple & 0x3F];
+    }
+
+    // Padding
+    for (i = 0; i < (3 - input_length % 3) % 3; i++) {
+        encoded_data[output_length - 1 - i] = '=';
+    }
+
+    encoded_data[output_length] = '\0';
+    return encoded_data;
 }
