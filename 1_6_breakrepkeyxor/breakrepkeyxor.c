fd5f4d8c6ec31d0a4dd621665e10b2fa4de6cbfc
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2024-12-03 14:12:33 +0200

    Continue with 1.6

--- 1_6_breakrepkeyxor/breakrepkeyxor.c
+++ 1_6_breakrepkeyxor/breakrepkeyxor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "../lib/hamming_distance.h"
 #include "../lib/hex_string_to_byte_array.h"
@@ -38,30 +39,69 @@ keysize_distance_t get_probable_keysize(int min_keysize, int max_keysize, char *
 	return kd;
 }
 
+char** split_string_in_chunks_of_length(char* input, int input_length, int chunk_size, int* num_chunks) 
+{
+	*num_chunks = (input_length + chunk_size - 1) / chunk_size;
+	char** chunks = malloc(*num_chunks * sizeof(char*));
+	
+	for (int i = 0; i < *num_chunks; i++) {
+		// allocate memory for each chunk
+		chunks[i] = malloc(chunk_size + 1); // +1 for null terminator
+		// Copy chunk from input string
+        int start = i * chunk_size;
+		int copy_length = (start + chunk_size < input_length) 
+			? chunk_size 
+			: input_length - start;
+
+		strncpy(chunks[i], input + start, copy_length);
+		chunks[i][copy_length] = '\0';  // Null-terminate the chunk	
+	}
+
+	return chunks;
+}
+
 int main(int argc, char *argv[])
 {
-    // usage, one arg, file name, is required
-    if (argc != 2)
-    {
-        printf("Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
+	// usage, one arg, file name, is required
+	if (argc != 2)
+	{
+		printf("Usage: %s <file>\n", argv[0]);
+		return 1;
+	}
 
-    // Read the file contents to a buffer
-    char *buffer = read_file(argv[1]);
-    if (buffer == NULL)
-    {
-        return 1;
-    }
+	// Read the file contents to a buffer
+	char *buffer = read_file(argv[1]);
+	if (buffer == NULL)
+	{
+		return 1;
+	}
 
-    // Decode the base64 encoded buffer
-    char *decoded = decode_base64(buffer);
+	// Decode the base64 encoded buffer
+	char *decoded = decode_base64(buffer);
 
-    // Get length of the decoded buffer
-    int decoded_length = strlen(decoded);
-        
-    keysize_distance_t kd = get_probable_keysize(2, 40, decoded, decoded_length); 
+	printf("Decoded string: %s\n", decoded);
 
-    printf("Keysize: %d\n", kd.keysize);
-    printf("Normalized distance: %f\n", kd.normalized_distance);    
+	// Get length of the decoded buffer
+	int decoded_length = strlen(decoded);
+
+	printf("decoded length %d\n", decoded_length);
+
+	keysize_distance_t kd = get_probable_keysize(2, 40, decoded, decoded_length); 
+
+	//  printf("Keysize: %d\n", kd.keysize);
+	//  printf("Normalized distance: %f\n", kd.normalized_distance);
+
+	int num_chunks = 0;
+	char **chunks = split_string_in_chunks_of_length(decoded, decoded_length, kd.keysize, &num_chunks);
+
+	printf("Number of chunks %d\n", num_chunks);
+
+	// Print chunks
+    if (chunks) {
+        for (size_t i = 0; i < num_chunks; i++) {
+            printf("Chunk %zu: %s\n", i, chunks[i]);
+            free(chunks[i]);  // Don't forget to free each chunk
+        }
+        free(chunks);  // Free the array of chunk pointers
+	}
 }
