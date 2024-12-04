23a024c7361ceb000a7e2ab31108544e82e58934
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2024-12-04 11:54:29 +0200

    COntinue

--- lib/decode_base64.c
+++ lib/decode_base64.c
@@ -9,13 +9,17 @@ char* decode_base64(const char *input, size_t *decoded_length) {
 
     size_t input_length = strlen(input);
     size_t output_length = (input_length / 4) * 3;
-    
+
+    // Adjust for padding characters
+    if (input[input_length - 1] == '=') output_length--;
+    if (input[input_length - 2] == '=') output_length--;
+
     char *output = (char *)malloc(output_length + 1); // +1 for null terminator
 
     if (output == NULL) {
         return NULL;
     }
-	
+    
     *decoded_length = base64_decode_block(input, input_length, output, &state);
     output[*decoded_length] = '\0';
     return output;
