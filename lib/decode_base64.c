a1e380025da36b1bf0c497831bfe58b5835db15a
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2024-12-04 10:51:48 +0200

    Fix

--- lib/decode_base64.c
+++ lib/decode_base64.c
@@ -9,9 +9,7 @@ char* decode_base64(const char *input, size_t *decoded_length) {
 
     size_t input_length = strlen(input);
     size_t output_length = (input_length / 4) * 3;
-    if (input[input_length - 1] == '=') output_length--;
-    if (input[input_length - 2] == '=') output_length--;
-
+    
     char *output = (char *)malloc(output_length + 1); // +1 for null terminator
 
     if (output == NULL) {
