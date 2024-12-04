cd7501b64a32bb163458d3aefe93a9d5525013d3
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2024-12-04 15:02:03 +0200

    Continue

---
+++ cp_dotnet/src/cp_dotnet.1_1_HexToBase64/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: HexToBase64 <hex_string>");
+            return;
+        }
+        
+        // read input as bytes
+        var input = args[0];
+        var bytes = StringUtils.StringToByteArray(input);
+        
+    }
+}
---
+++ cp_dotnet/src/cp_dotnet.Utils/StringUtils.cs
@@ -0,0 +1,16 @@
+namespace cp_dotnet.Utils
+{
+    public static class StringUtils
+    {
+        public static byte[] StringToByteArray(string input)
+        {
+            byte[] bytes = new byte[input.Length / 2];
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(input.Substring(i, 2), 16);
+            }
+
+            return bytes;
+        }
+    }
+}
