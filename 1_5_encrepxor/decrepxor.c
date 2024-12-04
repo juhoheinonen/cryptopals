ad620682d9436b535e0ec62804ea1b6764893dcb
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2024-12-04 13:51:35 +0200

    Remove base64 from decrepxor

--- 1_5_encrepxor/decrepxor.c
+++ 1_5_encrepxor/decrepxor.c
@@ -21,7 +21,6 @@ Encrypt a bunch of stuff using your repeating-key XOR function. Encrypt your mai
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../lib/decode_base64.h"
 #include "../lib/hex_string_to_byte_array.h"
 
 unsigned char *decrypt_repeating_key_xor(unsigned char *ciphertext, char *key, int ciphertext_len, int key_len)
@@ -45,36 +44,31 @@ unsigned char *decrypt_repeating_key_xor(unsigned char *ciphertext, char *key, i
 
 int main(int argc, char *argv[])
 {
-    char *hex_ciphertext;
-    char *base64_ciphertext;
-    char *key;
-    size_t hex_ciphertext_len;
-    unsigned char *ciphertext;
-    size_t ciphertext_len;
-
-    if (argc == 4 && strcmp(argv[1], "-b64") == 0)
+    // usage: ./decrepxor hex_ciphertext key
+    if (argc != 3)
     {
-        base64_ciphertext = argv[2];
-        size_t input_length = strlen(base64_ciphertext);                
-        ciphertext = decode_base64(base64_ciphertext, input_length, &ciphertext_len);                
-
-        key = argv[3];
-    }
-    else if (argc == 3)
-    {
-        hex_ciphertext = argv[1];
-        hex_ciphertext_len = strlen(hex_ciphertext);
-        key = argv[2];
-        *ciphertext = hex_string_to_byte_array(hex_ciphertext, hex_ciphertext_len);
-        *ciphertext_len = hex_ciphertext_len / 2;
-    }
-    else
-    {
-        printf("Usage: ./decrepxor hex_ciphertext key\n OR\nUsage: ./decrepxor -b64 base64_hex_ciphertext key\n");
+        printf("Usage: ./decrepxor hex_ciphertext key\n");
         return 1;
-    }           
+    }
 
-    unsigned char *plaintext = decrypt_repeating_key_xor(ciphertext, key, *ciphertext_len, strlen(key));
+    char *hex_ciphertext = argv[1];
+
+    // validate that the hex ciphertext is of even length
+    if (strlen(hex_ciphertext) % 2 != 0)
+    {
+        printf("Error: hex ciphertext must be of even length\n");
+        return 1;
+    }
+
+    char *key = argv[2];
+
+    int hex_ciphertext_len = strlen(hex_ciphertext);
+
+    unsigned char *ciphertext = hex_string_to_byte_array(hex_ciphertext, hex_ciphertext_len);
+
+    int ciphertext_len = hex_ciphertext_len / 2;
+
+    unsigned char *plaintext = decrypt_repeating_key_xor(ciphertext, key, ciphertext_len, strlen(key));
 
     printf("%s\n", plaintext);
 
