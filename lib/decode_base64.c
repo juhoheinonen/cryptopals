e6eb964e74152dee772db8c7c43be09f7c6251a5
Juho Antti Heinonen <juho.antti.heinonen@gmail.com>
2024-12-04 10:47:34 +0200

    Continue

--- lib/decode_base64.c
+++ lib/decode_base64.c
@@ -4,18 +4,22 @@
 #include <b64/cdecode.h>
 
 char* decode_base64(const char *input, size_t *decoded_length) {
-	base64_decodestate state;
-	base64_init_decodestate(&state);
+    base64_decodestate state;
+    base64_init_decodestate(&state);
 
-	size_t input_length = strlen(input);
-	size_t output_length = input_length * 3 / 4;
-	char *output = (char *)malloc(output_length + 1); // +1 for null terminator
+    size_t input_length = strlen(input);
+    size_t output_length = (input_length / 4) * 3;
+    if (input[input_length - 1] == '=') output_length--;
+    if (input[input_length - 2] == '=') output_length--;
 
-	if (output == NULL) {
-		return NULL;
-	}
+    char *output = (char *)malloc(output_length + 1); // +1 for null terminator
 
-	*decoded_length = base64_decode_block(input, input_length, output, &state);
-	output[*decoded_length] = '\0';
-	return output;
+    if (output == NULL) {
+        return NULL;
+    }
+
+	printf("here\n");
+    *decoded_length = base64_decode_block(input, input_length, output, &state);
+    output[*decoded_length] = '\0';
+    return output;
 }
